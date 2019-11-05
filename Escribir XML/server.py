a5959d32650cec4e6f997be83eb4513af1708bf0
Mario Martínez C <mmblu@MacBook-Pro-de-Mario.local>
2019-11-05 10:55:59 +0100

    Servidor de Echo

--- Escribir XML/server.py
+++ Escribir XML/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['POST', 'GET'])
-def ff():
-    print(request.data)
-    print(request.get_data())
-    return ('yas')
+def echo():
+    msg = request.data.decode('UTF-8')
+    return (f'Back to you: {msg}')
 
 app.run(host='0.0.0.0', debug=True, port='8484')
