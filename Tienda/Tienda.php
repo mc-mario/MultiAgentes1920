dc0e544c60306c897f9af48bc27b1af8a2f2dbb2
miguelean <33040293+miguelean@users.noreply.github.com>
2019-12-10 11:06:21 +0100

    Add files via upload

---
+++ Tienda/Tienda.php
@@ -0,0 +1,193 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+ 
+  
+  include_once 'funcionesGlobales.php';
+  include 'envios.php';
+  class Tienda extends funcionesGlobales
+	{
+	
+  	#Atributos
+		var $id ;
+			
+		#Constructor
+		function __construct($i){
+			$this->id=$i;
+			
+			#Escribimos en el log
+			funcionesGlobales::logs("logTienda.txt", "Abrimos una nueva tienda con identificador: " .$this->id);
+			
+		}
+		
+		#Funcion para entrar a la tienda 
+		function entrada($idDeTienda, $ipCliente, $idDeCliente, $cliente, $idMen, $horaEntrada, $idTiendaConocida, $ipTiendaConocida){
+			#Conectamos con BB.DD
+			$mysqli = funcionesGlobales::conectBBDD();
+			#Añadimos registro de entrada del cliente
+			$consult = "INSERT INTO tiendaCliente (idTienda, idCliente, idMensaje, horaMensaje, accion, productoVendido, cantidadProducto) VALUES ('$idDeTienda', '$cliente', '$idMen', '$horaEntrada', 'Dentro', NULL, NULL)";
+			$mysqli -> query($consult);
+			
+			#hay que coger tambien las tiendas que conoce el cliente
+      			foreach ($idTiendaConocida as $i => $value) {
+				$idTiendaC = $idTiendaConocida[$i];
+				$ipTiendaC = $ipTiendaConocida[$i];
+        $mysqli = funcionesGlobales::conectBBDD();
+          			$consult2 = "INSERT INTO tiendasConocidas (ipIdCliente, idTiendaDentro, idTiendaConocida, ipTiendaConocida) VALUES ('$cliente', '$idDeTienda', '$idTiendaC', '$ipTiendaC')";
+
+			   	$mysqli -> query($consult2);      
+					
+      			}
+			
+			#Mandamos mensaje a comprador
+			envios::envioDentro($idDeTienda, $ipCliente, $idDeCliente, $idMen, $horaEntrada);
+			
+		}
+		
+		#Funcion salida de tienda
+		function salida($idDeTienda, $cliente){
+			#Conectamos con BB.DD
+			$mysqli = funcionesGlobales::conectBBDD();
+			
+			#Buscamos en BB.DD registro de entrada
+			$result = $mysqli->query("SELECT * FROM tiendaCliente WHERE idCliente LIKE '$cliente' AND accion LIKE 'Dentro' "); 
+			$row = $result->fetch_assoc();
+			
+			#Eliminamos los registros pertenecientes a ese cliente
+			if($row != NULL){
+				$sql = "DELETE FROM tiendaCliente WHERE idCliente = '$cliente'";
+				$mysqli->query($sql);	
+			}
+      
+      			#Eliminamos las tiendas que conoce este cliente
+      			$sql = "DELETE FROM tiendasConocidas WHERE ipIdCliente = '$cliente'";
+			$mysqli->query($sql);	
+		}
+		
+		#vendemos un producto a un cliente
+		function venderProducto($idDeTienda, $ipCliente, $idCliente, $cliente, $idMen, $horaEntrada, $nombreProductos, $cantidades){
+			#Conectamos con BB.DD
+			$mysqli = funcionesGlobales::conectBBDD();
+			
+			#Comprobamos si el cliente esta dentro de la tienda
+			$sql = "SELECT * FROM tiendaCliente WHERE idTienda LIKE '$idDeTienda' AND idCliente LIKE '$cliente' AND accion LIKE 'Dentro'";
+			$result = $mysqli->query($sql);
+			$row = $result->fetch_assoc();
+			if($row != NULL){
+				$precios = array();
+				foreach ($nombreProductos as $i => $value) {
+					$nombreProducto = $nombreProductos[$i];
+					$cantidadP = $cantidades[$i];
+					#Vemos si el producto esta disponible
+					$sql = "SELECT * FROM tiendaProducto WHERE idTienda LIKE '$idDeTienda' AND nombreProducto LIKE '$nombreProducto' AND '$cantidadP' <= cantidad ";
+					$result = $mysqli->query($sql);
+					$row = $result->fetch_assoc();
+					if($row != NULL){
+						#Se actualiza la cantidad de la Tienda
+						$cant = $row["Cantidad"];
+						$nuevaCantidad = $cant - $cantidadP;
+						$sql = "UPDATE tiendaProducto
+									SET cantidad = '$nuevaCantidad'
+									WHERE nombreProducto LIKE '$nombreProducto' AND idTienda = '$idDeTienda' ";
+						$mysqli->query($sql);
+						
+						#Añadimos la compra
+						$consult = "INSERT INTO tiendaCliente (idTienda, idCliente, idMensaje, horaMensaje, accion, productoVendido, cantidadProducto) VALUES ('$idDeTienda', '$cliente', '$idMen', '$horaEntrada', 'Compra', '$nombreProducto', '$cantidadP')";
+						$mysqli -> query($consult);
+						
+						#Añadimos ganancias a la tienda
+						$precio = $row["Precio"];
+						$sql = "UPDATE tiendas
+									SET Dinero = Dinero + ('$precio' * '$cantidadP')
+									WHERE ID LIKE '$idDeTienda' ";
+						$mysqli->query($sql);
+						
+						array_push($precios, ($precio * $cantidadP));
+						
+						#Escribimos log
+						funcionesGlobales::logs("logTienda.txt", "Men: ".$idMen." -- Tienda: " .$idDeTienda. " : vende al comprador " .$cliente. " " .$cantidadP. " de " .$nombreProducto. " por " .($cantidadP * $precio). " euros");
+					}
+					else{
+						array_push($precios, 0);
+						echo "Producto: ".$nombreProducto." no disponible <br/>";
+					}
+				}
+
+				#Mandamos mensaje a comprador 
+				envios::envioVenta($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada, $nombreProductos, $cantidades, $precios);
+				
+			}
+			else {
+				###############################################
+				#Mandamos mensaje a comprador OJO FALTA ESTOOOOOOO!!!!!!!!!!!!!!!!!!!
+				###############################################
+				echo "El comprador no esta en la tienda";
+			}	
+		}
+		
+		#pasamos una lista de productos disponibles al cliente
+		function listaCompra($idDeTienda, $ipCliente, $idCliente, $cliente, $idMen, $horaEntrada, $nombreProductos, $cantidades){
+			#Conectamos con BB.DD
+			$mysqli = funcionesGlobales::conectBBDD();
+			
+			#Comprobamos si el cliente esta dentro de la tienda
+			$sql = "SELECT * FROM tiendaCliente WHERE idTienda LIKE '$idDeTienda' AND idCliente LIKE '$cliente' AND accion LIKE 'Dentro'";
+			$result = $mysqli->query($sql);
+			$row = $result->fetch_assoc();
+			if($row != NULL){
+				$precios = array();
+				foreach ($nombreProductos as $i => $value) {
+					$nombreProducto = $nombreProductos[$i];
+					$cantidadP = $cantidades[$i];
+					#Vemos si el producto esta disponible
+					$sql = "SELECT * FROM tiendaProducto WHERE idTienda LIKE '$idDeTienda' AND idProducto LIKE '$nombreProducto' AND '$cantidadP' <= cantidad ";
+					$result = $mysqli->query($sql);
+					$row = $result->fetch_assoc();
+					if($row != NULL){
+						$precio = $row["Precio"];
+						array_push($precios, ($precio * $cantidadP));
+						#Escribimos log
+						funcionesGlobales::logs("logTienda.txt", "Men: ".$idMen." -- Tienda: " .$idDeTienda. " : pasamos lista a " .$cliente. " " .$cantidadP. " de " .$nombreProducto. " por " .($cantidadP * $precio). " euros");
+					}
+					else{
+						array_push($precios, 0);
+						echo "Producto: ".$nombreProducto." no disponible <br/>";
+					}
+				}
+				#Mandamos mensaje a comprador 
+				envios::envioLista($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada, $nombreProductos, $cantidades, $precios);
+				
+			}
+			else {
+				###############################################
+				#Mandamos mensaje a comprador OJO FALTA ESTOOOOOOO!!!!!!!!!!!!!!!!!!!
+				###############################################
+				echo "El comprador no esta en la tienda";
+			}	
+		}
+		
+		#Funcion para cerrar la tienda 
+		function cerrar($a){
+			
+			#Conectamos con BB.DD
+			$mysqli = funcionesGlobales::conectBBDD();
+			
+			#Eliminamos la relacion entre producto-tienda y cliente-tienda
+			$sql = "DELETE FROM tiendaProducto WHERE idTienda = '$a'";
+			$mysqli->query($sql);
+			
+			$sql = "DELETE FROM tiendacliente WHERE idTienda = '$a'";
+			$mysqli->query($sql);
+			
+			#Escribimos en el log
+			funcionesGlobales::logs("logTienda.txt", "Cerramos la tienda con identificador: " .$a);
+		}
+		
+		#Dar las tiendas que conocen los demas clientes
+		function dameTienda($a, $cliente, $ipCliente, $idCliente, $idMen, $horaEntrada){
+			  envios::envioTienda($a, $ipCliente, $idCliente, $idMen, $horaEntrada);
+		}
+	}
+?>
---
+++ Tienda/envios.php
@@ -0,0 +1,126 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+class envios{
+	function envioVenta($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada, $nombreProductos, $cantidades, $precios){
+		
+		$ventas = simplexml_load_file('/var/www/html/mensajes/Ventas.xml');
+		
+		#idMensaje
+		$idMensaje = $ventas->cabecera->addChild('idMensaje', $idMen+1);
+		
+		#Emisor
+		$ipEmisor = $ventas->cabecera->identificacionEmisor->addChild('ipEmisor', "63.33.189.149");
+		$idEmisor = $ventas->cabecera->identificacionEmisor->addChild('idEmisor', $idDeTienda);
+		
+		#Receptor
+		$ipReceptor = $ventas->cabecera->identificacionReceptor->addChild('ipReceptor', $ipCliente);
+		$idReceptor = $ventas->cabecera->identificacionReceptor->addChild('idReceptor', $idCliente);
+		
+		#TipoMensaje
+		$tipoMensaje = $ventas->cabecera->addChild('tipoMensaje', 'TPV');
+		
+		#Horas
+		$horaEnvio = $ventas->cabecera->addChild('horaEnvio', '00:00');
+		$horaRecepcion = $ventas->cabecera->addChild('horaRecepcion', '00:01');
+		
+		#Lista productos
+		foreach ($precios as $z => $value) {
+			if ($precios[$z] != 0){
+				$producto[$z] = $ventas->cuerpo->listaCompra->addChild('producto');
+				$nombre = $ventas->cuerpo->listaCompra->producto[$z]->addChild('idProducto', $nombreProductos[$z]);
+				$cantidad = $ventas->cuerpo->listaCompra->producto[$z]->addChild('cantidad', $cantidades[$z]);
+				$precio = $ventas->cuerpo->listaCompra->producto[$z]->addChild('precio', $precios[$z]);
+			}
+		}
+		echo $ventas->asXML();
+	}
+	
+	function envioDentro($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada){
+		$dentro = simplexml_load_file('/var/www/html/mensajes/Dentro.xml');
+		
+		#idMensaje
+		$idMensaje = $dentro->cabecera->addChild('idMensaje', $idMen+1);
+		
+		#Emisor
+		$ipEmisor = $dentro->cabecera->identificacionEmisor->addChild('ipEmisor', '63.33.189.149');
+		$idEmisor = $dentro->cabecera->identificacionEmisor->addChild('idEmisor', $idDeTienda);
+		
+		#Receptor
+		$ipReceptor = $dentro->cabecera->identificacionReceptor->addChild('ipReceptor', $ipCliente);
+		$idReceptor = $dentro->cabecera->identificacionReceptor->addChild('idReceptor', $idCliente);
+		
+		#TipoMensaje
+		$tipoMensaje = $dentro->cabecera->addChild('tipoMensaje', 'TAAT');
+		
+		#Horas
+		$horaEnvio = $dentro->cabecera->addChild('horaEnvio', '00:00');
+		$horaRecepcion = $dentro->cabecera->addChild('horaRecepcion', '00:01');
+
+		echo $dentro->asXML();
+	}
+ 
+ function envioTienda($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada){
+		
+		$tiendas = simplexml_load_file('/var/www/html/mensajes/Tiendas.xml');
+		
+		#idMensaje
+		$idMensaje = $tiendas->cabecera->addChild('idMensaje', $idMen+1);
+		
+		#Emisor
+		$ipEmisor = $tiendas->cabecera->identificacionEmisor->addChild('ipEmisor', "63.33.189.149");
+		$idEmisor = $tiendas->cabecera->identificacionEmisor->addChild('idEmisor', $idDeTienda);
+		
+		#Receptor
+		$ipReceptor = $tiendas->cabecera->identificacionReceptor->addChild('ipReceptor', $ipCliente);
+		$idReceptor = $tiendas->cabecera->identificacionReceptor->addChild('idReceptor', $idCliente);
+		
+		#TipoMensaje
+		$tipoMensaje = $tiendas->cabecera->addChild('tipoMensaje', 'TTT');
+		
+		#Horas
+		$horaEnvio = $tiendas->cabecera->addChild('horaEnvio', '00:00');
+		$horaRecepcion = $tiendas->cabecera->addChild('horaRecepcion', '00:01');
+		
+		
+		echo $tiendas->asXML();
+	}
+ 
+ function envioLista($idDeTienda, $ipCliente, $idCliente, $idMen, $hora, $nombreProductos, $cantidades, $precios){
+		
+		$lista = simplexml_load_file('/var/www/html/mensajes/Lista.xml');
+		
+		#idMensaje
+		$idMensaje = $lista->cabecera->addChild('idMensaje', $idMen+1);
+		
+		#Emisor
+		$ipEmisor = $lista->cabecera->identificacionEmisor->addChild('ipEmisor', "63.33.189.149");
+		$idEmisor = $lista->cabecera->identificacionEmisor->addChild('idEmisor', $idDeTienda);
+		
+		#Receptor
+		$ipReceptor = $lista->cabecera->identificacionReceptor->addChild('ipReceptor', $ipCliente);
+		$idReceptor = $lista->cabecera->identificacionReceptor->addChild('idReceptor', $idCliente);
+		
+		#TipoMensaje
+		$tipoMensaje = $lista->cabecera->addChild('tipoMensaje', 'TCT');
+		
+		#Horas
+		$horaEnvio = $lista->cabecera->addChild('horaEnvio', $hora);
+		$horaRecepcion = $lista->cabecera->addChild('horaRecepcion', '00:01');
+		
+		#Lista productos
+		foreach ($precios as $z => $value) {
+			if ($precios[$z] != 0){
+				$producto[$z] = $lista->cuerpo->listaCompra->addChild('producto');
+				$nombre = $lista->cuerpo->listaCompra->producto[$z]->addChild('idProducto', $nombreProductos[$z]);
+				$cantidad = $lista->cuerpo->listaCompra->producto[$z]->addChild('cantidad', $cantidades[$z]);
+				$precio = $lista->cuerpo->listaCompra->producto[$z]->addChild('precio', $precios[$z]);
+			}
+		}
+		echo $lista->asXML();
+	}
+}
+?>
