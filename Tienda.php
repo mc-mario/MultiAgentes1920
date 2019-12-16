158144a0a9c7a04cf18c4f5ea91a2490cf28bc51
Arturo0712 <56581287+Arturo0712@users.noreply.github.com>
2019-12-16 02:59:51 +0100

    Tienda que vende por precio

--- Tienda.php
+++ Tienda.php
@@ -1,12 +1,12 @@
 <?php
-  
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+ 
   
   include_once 'funcionesGlobales.php';
   include 'envios.php';
-  header('Access-Control-Allow-Origin: *');
-  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-	
   class Tienda extends funcionesGlobales
 	{
 	
@@ -23,34 +23,41 @@
 		}
 		
 		#Funcion para entrar a la tienda 
-		function entrada($idDeTienda, $ipCliente, $idDeCliente, $cliente, $idMen, $horaEntrada){
+		function entrada($idDeTienda, $ipCliente, $idDeCliente, $cliente, $idMen, $horaEntrada, $idTiendaConocida, $ipTiendaConocida){
 			#Conectamos con BB.DD
 			$mysqli = funcionesGlobales::conectBBDD();
 			#Añadimos registro de entrada del cliente
-			$consult = "INSERT INTO tiendacliente (idTienda, idCliente, idMensaje, horaMensaje, accion, productoVendido, cantidadProducto) VALUES ('$idDeTienda', '$cliente', '$idMen', '$horaEntrada', 'Dentro', NULL, NULL)";
+			$consult = "INSERT INTO tiendaCliente (idTienda, idCliente, idMensaje, horaMensaje, accion, productoVendido, cantidadProducto) VALUES ('$idDeTienda', '$cliente', '$idMen', '$horaEntrada', 'Dentro', NULL, NULL)";
 			$mysqli -> query($consult);
-			funcionesGlobales::consultaTiendaCliente('insert', $idDeTienda, $cliente, $idMen, $horaEntrada, 'Dentro', NULL, NULL);
-			#hay que coger tambien las tiendas que conoce el cliente
 			
+			#hay que coger tambien las tiendas que conoce el cliente
+      			foreach ($idTiendaConocida as $i => $value) {
+				$idTiendaC = $idTiendaConocida[$i];
+				$ipTiendaC = $ipTiendaConocida[$i];
+        			$mysqli = funcionesGlobales::conectBBDD();
+          			$consult2 = "INSERT INTO tiendasConocidas (ipIdCliente, idTiendaDentro, idTiendaConocida, ipTiendaConocida) VALUES ('$cliente', '$idDeTienda', '$idTiendaC', '$ipTiendaC')";
+
+			   	$mysqli -> query($consult2);      
+					
+      			}
 			#Mandamos mensaje a comprador
 			envios::envioDentro($idDeTienda, $ipCliente, $idDeCliente, $idMen, $horaEntrada);
 			
 		}
 		
 		#Funcion salida de tienda
-		function salida($idDeTienda, $idDeCliente){
+		function salida($idDeTienda, $cliente){
 			#Conectamos con BB.DD
 			$mysqli = funcionesGlobales::conectBBDD();
-			
-			#Buscamos en BB.DD registro de entrada
-			$result = $mysqli->query("SELECT * FROM tiendacliente WHERE idCliente LIKE '$idDeCliente' AND accion LIKE 'Dentro' "); 
-			$row = $result->fetch_assoc();
+			funcionesGlobales::logs("logTienda.txt", "Sale la tienda: ".$cliente);
 			
 			#Eliminamos los registros pertenecientes a ese cliente
-			if($row != NULL){
-				$sql = "DELETE FROM tiendacliente WHERE idCliente = '$idDeCliente'";
+				$sql = "DELETE FROM tiendaCliente WHERE idCliente = '$cliente'";
 				$mysqli->query($sql);	
-			}
+      
+      			#Eliminamos las tiendas que conoce este cliente
+      			$sql = "DELETE FROM tiendasConocidas WHERE ipIdCliente = '$cliente'";
+			$mysqli->query($sql);	
 		}
 		
 		#vendemos un producto a un cliente
@@ -59,37 +66,51 @@
 			$mysqli = funcionesGlobales::conectBBDD();
 			
 			#Comprobamos si el cliente esta dentro de la tienda
-			$sql = "SELECT * FROM tiendacliente WHERE idTienda LIKE '$idDeTienda' AND idCliente LIKE '$cliente' AND accion LIKE 'Dentro'";
+			$sql = "SELECT * FROM tiendaCliente WHERE idTienda LIKE '$idDeTienda' AND idCliente LIKE '$cliente' AND accion LIKE 'Dentro'";
 			$result = $mysqli->query($sql);
 			$row = $result->fetch_assoc();
-			var_dump($row);
 			if($row != NULL){
 				$precios = array();
 				foreach ($nombreProductos as $i => $value) {
 					$nombreProducto = $nombreProductos[$i];
 					$cantidadP = $cantidades[$i];
 					#Vemos si el producto esta disponible
-					$sql = "SELECT * FROM tiendaproducto WHERE idTienda LIKE '$idDeTienda' AND nombreProducto LIKE '$nombreProducto' AND '$cantidadP' <= cantidad ";
+					$sql = "SELECT * FROM tiendaProducto WHERE idTienda LIKE '$idDeTienda' AND idProducto LIKE '$nombreProducto' AND '$cantidadP' <= cantidad ";
 					$result = $mysqli->query($sql);
 					$row = $result->fetch_assoc();
 					if($row != NULL){
 						#Se actualiza la cantidad de la Tienda
 						$cant = $row["Cantidad"];
 						$nuevaCantidad = $cant - $cantidadP;
-						$sql = "UPDATE tiendaProducto
-									SET cantidad = '$nuevaCantidad'
-									WHERE nombreProducto LIKE '$nombreProducto' AND idTienda = '$idDeTienda' ";
+						$sql = "UPDATE tiendaProducto SET cantidad = '$nuevaCantidad' WHERE nombreProducto LIKE '$nombreProducto' AND idTienda = '$idDeTienda' ";
 						$mysqli->query($sql);
 						
 						#Añadimos la compra
-						$consult = "INSERT INTO tiendacliente (idTienda, idCliente, idMensaje, horaMensaje, accion, productoVendido, cantidadProducto) VALUES ('$idDeTienda', '$cliente', '$idMen', '$horaEntrada', 'Compra', '$nombreProducto', '$cantidadP')";
+						$consult = "INSERT INTO tiendaCliente (idTienda, idCliente, idMensaje, horaMensaje, accion, productoVendido, cantidadProducto) VALUES ('$idDeTienda', '$cliente', '$idMen', '$horaEntrada', 'Compra', '$nombreProducto', '$cantidadP')";
 						$mysqli -> query($consult);
 						
 						#Añadimos ganancias a la tienda
 						$precio = $row["Precio"];
-						$sql = "UPDATE tiendas
-									SET Dinero = Dinero + ('$precio' * '$cantidadP')
-									WHERE ID LIKE '$idDeTienda' ";
+						#Establecemos el precio según la cantidad disponible
+						if($cant > 200){
+							if($cant > 300){
+								$precio = intval($precio*0.5);
+							}
+							else{
+								$porcentajeBajada = ($cant-200)/200 #un valor de 0 a 0.5
+								$precio = intval($precio*(1-$porcentajeBajada));
+							}
+						}
+						else if($cant <= 100){
+							if($cant < 20){
+								$precio = intval($precio*1.5);
+							}
+							else{
+								$porcentajeSubida = ($cant-20)/160 #un valor de 0 a 0.5
+								$precio = intval($precio*(1.5-$porcentajeSubida));#En este caso restamos el porcentaje porque la proporción es inversa
+							}
+						}
+						$sql = "UPDATE tiendas SET Dinero = Dinero + ('$precio' * '$cantidadP') WHERE ID LIKE '$idDeTienda' ";
 						$mysqli->query($sql);
 						
 						array_push($precios, ($precio * $cantidadP));
@@ -97,14 +118,68 @@
 						#Escribimos log
 						funcionesGlobales::logs("logTienda.txt", "Men: ".$idMen." -- Tienda: " .$idDeTienda. " : vende al comprador " .$cliente. " " .$cantidadP. " de " .$nombreProducto. " por " .($cantidadP * $precio). " euros");
 					}
-					else{
-						array_push($precios, 0);
-						echo "Producto: ".$nombreProducto." no disponible <br/>";
-					}
 				}
 
 				#Mandamos mensaje a comprador 
 				envios::envioVenta($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada, $nombreProductos, $cantidades, $precios);
+				
+			}
+			else {
+				###############################################
+				#Mandamos mensaje a comprador OJO FALTA ESTOOOOOOO!!!!!!!!!!!!!!!!!!!
+				###############################################
+				 "El comprador no esta en la tienda";
+			}	
+		}
+		
+		#pasamos una lista de productos disponibles al cliente
+		function listaCompra($idDeTienda, $ipCliente, $idCliente, $cliente, $idMen, $horaEntrada, $nombreProductos, $cantidades){
+			#Conectamos con BB.DD
+			$mysqli = funcionesGlobales::conectBBDD();
+			
+			#Comprobamos si el cliente esta dentro de la tienda
+			$sql = "SELECT * FROM tiendaCliente WHERE idTienda LIKE '$idDeTienda' AND idCliente LIKE '$cliente' AND accion LIKE 'Dentro'";
+			$result = $mysqli->query($sql);
+			$row = $result->fetch_assoc();
+			if($row != NULL){
+				$precios = array();
+				foreach ($nombreProductos as $i => $value) {
+					$nombreProducto = $nombreProductos[$i];
+					$cantidadP = $cantidades[$i];
+					#Vemos si el producto esta disponible
+					$sql = "SELECT * FROM tiendaProducto WHERE idTienda LIKE '$idDeTienda' AND idProducto LIKE '$nombreProducto' AND '$cantidadP' <= cantidad ";
+					$result = $mysqli->query($sql);
+					$row = $result->fetch_assoc();
+					if($row != NULL){
+						$cant = $row["Cantidad"];
+						$precio = $row["Precio"];
+						#Establecemos el precio según la cantidad disponible
+						if($cant > 200){
+							if($cant > 300){
+								$precio = intval($precio*0.5);
+							}
+							else{
+								$porcentajeBajada = ($cant-200)/200 #un valor de 0 a 0.5
+								$precio = intval($precio*(1-$porcentajeBajada));
+							}
+						}
+						else if($cant <= 100){
+							if($cant < 20){
+								$precio = intval($precio*1.5);
+							}
+							else{
+								$porcentajeSubida = ($cant-20)/160 #un valor de 0 a 0.5
+								$precio = intval($precio*(1.5-$porcentajeSubida));#En este caso restamos el porcentaje porque la proporción es inversa
+							}
+						}
+						array_push($precios, ($precio * $cantidadP));
+						#Escribimos log
+						funcionesGlobales::logs("logTienda.txt", "Men: ".$idMen." -- Tienda: " .$idDeTienda. " : pasamos lista a " .$cliente. " " .$cantidadP. " de " .$nombreProducto. " por " .($cantidadP * $precio). " euros");
+					}
+									
+				}
+				#Mandamos mensaje a comprador 
+				envios::envioLista($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada, $nombreProductos, $cantidades, $precios);
 				
 			}
 			else {
@@ -133,10 +208,34 @@
 		}
 		
 		#Dar las tiendas que conocen los demas clientes
-		function dameTienda($a){
-			###############################################
-			#Mandamos mensaje a comprador OJO FALTA ESTOOOOOOO!!!!!!!!!!!!!!!!!!!
-			###############################################
+		function dameTienda($tienda, $cliente, $ipCliente, $idCliente, $idMen, $horaEntrada){
+
+			funcionesGlobales::logs("logHotel.txt", "Comprobamos tiendas conocidas");
+			$mysqli = funcionesGlobales::conectBBDD();
+			$sql = "SELECT ipTiendaConocida FROM tiendasConocidas";
+                        $ipResult = $mysqli->query($sql);
+			$mysqli = funcionesGlobales::conectBBDD();
+                        $sql = "SELECT idTiendaConocida FROM tiendasConocidas";
+                        $idResult = $mysqli->query($sql);
+			$mysqli = funcionesGlobales::conectBBDD();
+                        $sql = "SELECT ipIdCliente FROM tiendasConocidas";
+                        $compradorResult = $mysqli->query($sql);
+                        
+			$ipResultArray = array();
+			$idResultArray = array();
+      $compradores = array();
+			while($row = mysqli_fetch_assoc($ipResult)){
+				$ipResultArray[] = $row["ipTiendaConocida"];
+			}
+      while($row = mysqli_fetch_assoc($compradorResult)){
+				$compradores[] = $row["ipIdCliente"];
+			}
+			while($row = mysqli_fetch_assoc($idResult)){
+                                $idResultArray[] = $row["idTiendaConocida"];
+                        }
+
+            funcionesGlobales::logs("logHotel.txt", "Solicitamos el envio de mensaje");
+			envios::envioTienda($idDeTienda, $ipCliente, $idCliente, $idMen, $horaEntrada, $ipResultArray, $idResultArray, $compradores);
 		}
 	}
 ?>
